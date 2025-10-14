bec337ce6467cce3ab0d3841f3609d839f9cd7f5
Chris Najman <christopher.najman@gmail.com>
2025-10-14 13:14:36 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# Bad Date-picker
+
+My entry for the [Bad UX World Cup](https://badux.lol/).
+
+[View on GitHub Pages](https://chrisnajman.github.io/bad-date-picker)
---
+++ index.js
@@ -0,0 +1,9 @@
+import themeSwitcher from "./js-modules/theme.js"
+import loadingAnimation from "./js-modules/loader.js"
+import details from "./js-modules/details.js"
+import badDatePicker from "./js-modules/bad-date-picker.js"
+
+themeSwitcher()
+loadingAnimation()
+details()
+badDatePicker()
